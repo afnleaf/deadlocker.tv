b8d3e5d898ab70929306fb42f6f4cd7cacebdc97
afnleaf <afnleafow@gmail.com>
2024-09-25 18:26:01 -0400

    fixed port

--- webapp/src/main.go
+++ webapp/src/main.go
@@ -10,8 +10,8 @@ func main() {
     fs := http.FileServer(http.Dir("./public"))
     http.Handle("/", fs)
 
-    log.Println("Serving on http://localhost:8080")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
+    log.Println("Serving on http://localhost:4444")
+    if err := http.ListenAndServe(":4444", nil); err != nil {
         log.Fatal(err)
     }
 }
